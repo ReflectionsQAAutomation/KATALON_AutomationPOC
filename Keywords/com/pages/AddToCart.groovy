eafd8eeab1d58cdcd877d03dadc155b55dffbc0e
roshnig <roshnig@REFL-LP216.reflections.local>
2024-03-02 16:56:32 +0530

    Demo

--- Keywords/com/pages/AddToCart.groovy
+++ Keywords/com/pages/AddToCart.groovy
@@ -30,10 +30,14 @@ public class AddToCart {
 		int aScreen = WebUI.getPageWidth()
 		WebUI.waitForPageLoad(10)
 		WebUI.click(findTestObject('Object Repository/Checkout/Page_Practice Software Testing - Toolshop - v5.0/button_Add to cart'))
-		WebUI.waitForElementVisible(findTestObject('Object Repository/Checkout/Page_Practice Software Testing - Toolshop - v5.0/div_Product added to shopping cart'), 10)
 		if (!(aScreen < 1000)) {
-		WebUI.verifyElementText(findTestObject('Object Repository/Checkout/Page_Practice Software Testing - Toolshop - v5.0/div_Product added to shopping cart'),'Product added to shopping cart.')
+			WebUI.delay(1)
 		}
+		else
+		{
+			WebUI.waitForElementVisible(findTestObject('Object Repository/Checkout/Page_Practice Software Testing - Toolshop - v5.0/div_Product added to shopping cart'), 10)
+		}
+		WebUI.verifyElementText(findTestObject('Object Repository/Checkout/Page_Practice Software Testing - Toolshop - v5.0/div_Product added to shopping cart'),'Product added to shopping cart.')
 		WebUI.delay(5)
 	}
 
