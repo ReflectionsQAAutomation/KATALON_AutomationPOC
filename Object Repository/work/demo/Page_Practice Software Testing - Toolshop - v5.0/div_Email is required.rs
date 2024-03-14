963af1b7b6a8e0e017588e6b3b8fe39a255492c6
roshnig <roshnig@REFL-LP216.reflections.local>
2024-03-14 11:39:00 +0530

    Lambdatest integration  change

---
+++ Object Repository/work/demo/Page_Practice Software Testing - Toolshop - v5.0/div_Email is required.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Email is required</name>
+   <tag></tag>
+   <elementGuidId>511f0e3c-363d-40c9-b8b7-4ec06e15eec8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='email_alert']/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#email_alert > div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>672bb30d-329c-4b6d-8fcb-6d48fbb04c3d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Email is required</value>
+      <webElementGuid>e4aa80de-fd44-4775-9e90-3371cb292d3c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;email_alert&quot;)/div[1]</value>
+      <webElementGuid>b3891bfd-a584-4b5a-b157-2e7152cf42a1</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='email_alert']/div</value>
+      <webElementGuid>6813e65f-9ad6-47a2-a4d9-360e850805f6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Email address'])[1]/following::div[2]</value>
+      <webElementGuid>821fae74-992c-4045-95f8-ee1bc291f1e4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Last name is required'])[1]/following::div[3]</value>
+      <webElementGuid>feddb9ea-a7ec-41b7-876d-73c30bc4579e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Subject'])[1]/preceding::div[1]</value>
+      <webElementGuid>ba0b32cf-00ff-4dec-a0c3-4f83d8802168</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Subject is required'])[1]/preceding::div[2]</value>
+      <webElementGuid>ab5b0472-90ad-4649-954f-cc85e42d1296</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Email is required']/parent::*</value>
+      <webElementGuid>87bf0b75-2c34-425e-8333-b27dc58760e5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/div</value>
+      <webElementGuid>fd5b39a5-6b98-44c1-a9ac-9121d1ece870</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Email is required' or . = 'Email is required')]</value>
+      <webElementGuid>9773e77f-69e0-48ee-abd3-7953ae04afcd</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
